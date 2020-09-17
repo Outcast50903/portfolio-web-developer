7bfb1ed37bba96875bd58adb4622fe1517463fca
Outcast50903 <dgarciavallejos@gmail.com>
2020-09-17 11:26:20 -0300

    Updates

---
+++ precache-manifest.7dfc15063466511e5671d0f48d50d0eb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9d2d24b4bc4f7ee29c08488b87a94e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2686ba2f9c1cb5fa7d6",
+    "url": "/static/css/2.52780007.chunk.css"
+  },
+  {
+    "revision": "a3cdb321f12972b1b670",
+    "url": "/static/css/main.b0da7611.chunk.css"
+  },
+  {
+    "revision": "e2686ba2f9c1cb5fa7d6",
+    "url": "/static/js/2.b049f8d6.chunk.js"
+  },
+  {
+    "revision": "788d1a44baecef2d1a1261c8e91b6967",
+    "url": "/static/js/2.b049f8d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3cdb321f12972b1b670",
+    "url": "/static/js/main.393876b4.chunk.js"
+  },
+  {
+    "revision": "9e106bc752f003e58925",
+    "url": "/static/js/runtime-main.835b0327.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
